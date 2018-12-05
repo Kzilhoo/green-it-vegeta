f840cf7aa71095d80262b9d4d6eece6839277376
Adrien Excoffier <adrien.excoffier@gmail.com>
2018-12-05 10:16:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+const vegeta = require('node-vegeta');
+const Attack = vegeta.Attack;
+const testAttack = new Attack();
+const fs = require('fs');
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/attack',
+    handler: function (request, h) {
+        var promiseMonCul = new Promise(function (resolve, reject) {
+            const wstream = fs.createWriteStream('targets.txt');
+            wstream.write(request.query.method + ' ' + request.query.url);
+            wstream.end(function () {
+                resolve();
+            });
+        });
+
+        return promiseMonCul.then(() => {
+            testAttack
+                .targets('targets.txt')
+                .rate(300)
+                .duration(request.query.duration + 's')
+                .report()
+                .process()
+                .stdout.on('data', (data) => {
+                    console.log('report', data.toString());
+                })
+            return h.redirect(process.env.REDIRECTION);
+
+        })
+
+    }
+});
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
